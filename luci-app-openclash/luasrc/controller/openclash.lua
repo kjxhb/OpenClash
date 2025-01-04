05adba04cb9fdd907a4ac6bdfeb164cd16281fd8
kjrzxu <xhb1011@163.com>
2025-01-04 11:52:12 +0800

    更新文件

--- luci-app-openclash/luasrc/controller/openclash.lua
+++ luci-app-openclash/luasrc/controller/openclash.lua
@@ -7,7 +7,7 @@ function index()
 
 	local page
 	
-	page = entry({"admin", "services", "openclash"}, alias("admin", "services", "openclash", "client"), _("OpenClash"), 50)
+	page = entry({"admin", "services", "openclash"}, alias("admin", "services", "openclash", "client"), _("ProxyOpenClash"), 50)
 	page.dependent = true
 	page.acl_depends = { "luci-app-openclash" }
 	entry({"admin", "services", "openclash", "client"},form("openclash/client"),_("Overviews"), 20).leaf = true
